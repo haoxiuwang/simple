b1aede783ee55844b1f344509865ef8f8e06fbd0
feiwang <fei.aomoxo@gmail.com>
2025-07-21 15:52:30 +0800

    update

--- app/src/main/java/com/example/simplewelcome/MainActivity.java
+++ app/src/main/java/com/example/simplewelcome/MainActivity.java
@@ -7,6 +7,7 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private WebServer server;
     @Override
