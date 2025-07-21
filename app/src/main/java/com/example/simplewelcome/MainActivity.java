4aae8d121043c9b7801cf0cfd6e9c8825a73a919
feiwang <fei.aomoxo@gmail.com>
2025-07-21 14:06:03 +0800

    update

--- app/src/main/java/com/example/simplewelcome/MainActivity.java
+++ app/src/main/java/com/example/simplewelcome/MainActivity.java
@@ -5,9 +5,23 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+    private Webserver server
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        server = new WebServer(this, 8080);
+        try {
+            server.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (server != null) {
+            server.stop();
+        }
     }
 }
