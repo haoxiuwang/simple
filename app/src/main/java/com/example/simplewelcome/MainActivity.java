a14b5634779387db6b6656e622e07460f10ce160
feiwang <fei.aomoxo@gmail.com>
2025-07-25 23:16:54 +0800

    update

--- app/src/main/java/com/example/simplewelcome/MainActivity.java
+++ app/src/main/java/com/example/simplewelcome/MainActivity.java
@@ -19,6 +19,8 @@ import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
     private WebServer server;
+    private WebView webView;
+    private FrameLayout overlay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
